2ee16f7751e724959f12165f02fa975648c3bfc1
olegzhr <14097456+olegzhr@users.noreply.github.com>
2021-08-18 03:45:57 +0300

    Update install_centos7.sh

--- install_centos7.sh
+++ install_centos7.sh
@@ -267,14 +267,15 @@ sudo cp $INSTALL_PATH/configs/logback.xml $GLASSFISH_PATH/glassfish/domains/doma
 
 echo "* Installion Alertflex applications *"  
 cd $INSTALL_PATH
-git clone https://github.com/alertflex/mc.git
+
 sudo mvn package
 
 sudo $GLASSFISH_PATH/bin/asadmin --passwordfile password.txt --user $ADMIN_USER deploy controller/target/alertflex-ctrl.war
 
 if [[ $INSTALL_MC == yes ]]
 then
-	sudo $GLASSFISH_PATH/bin/asadmin --passwordfile password.txt --user $ADMIN_USER deploy mc/target/alertflex-mc.war
+	sudo curl -LO "https://github.com/alertflex/cnode/releases/download/v1.0.1/alertflex-mc.war"
+	sudo $GLASSFISH_PATH/bin/asadmin --passwordfile password.txt --user $ADMIN_USER deploy alertflex-mc.war
 fi
 
 sudo /etc/init.d/payara_domain1 restart
